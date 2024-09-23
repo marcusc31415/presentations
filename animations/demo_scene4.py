3869a975aa8e924d81dcb2055069f05b61d299fe
marcusc31415 <darthmonks@gmail.com>
2024-09-23 18:27:14 +1000

    Scale Stuff

---
+++ animations/demo_scene4.py
@@ -0,0 +1,109 @@
+import manim as mn
+import numpy as np
+from manim_revealjs import PresentationScene, COMPLETE_LOOP
+
+
+mn.config.video_dir= "./videos"
+
+class DemoScene4(PresentationScene):
+    def construct(self):
+            
+        self.play(mn.Write(tex := mn.Tex("The Scale ", "and ", r"\textbf{STUFF}")))
+        self.end_fragment()
+
+        animations = [
+                mn.FadeOut(tex[0]),
+                mn.FadeOut(tex[1], shift=mn.DOWN + mn.LEFT),
+                mn.FadeOut(tex[2], scale=0.5)
+                ]
+
+        self.play(mn.AnimationGroup(*animations, lag_ratio=0.5))
+        self.end_fragment()
+
+        # Display the group in the centre. 
+        circle_1 = mn.Circle(radius=3, color=mn.PURE_GREEN, stroke_color=mn.WHITE, fill_opacity=0.6)
+        circle_1_label = mn.Tex("$G$")
+        circle_1_label.move_to(circle_1.get_center() + (mn.UP + mn.LEFT)*3/np.sqrt(2)*1.2)
+        self.play(mn.Create(circle_1), mn.Write(circle_1_label))
+        self.end_fragment()
+
+
+        rect_1 = mn.Rectangle(height=1, width=2, fill_color=mn.GREY, fill_opacity=1)
+        rect_1.move_to(mn.DOWN)
+        brace_right = mn.BraceLabel(rect_1, "U_+", brace_direction=mn.RIGHT)
+        brace_down = mn.BraceLabel(rect_1, "U_-", brace_direction=mn.DOWN)
+        corner = rect_1.get_corner(mn.UP + mn.LEFT)
+        subgroup_label = mn.Tex("$U$")
+        subgroup_label.move_to(corner + (mn.UP + mn.LEFT)*0.2)
+
+        self.play(mn.Create(rect_1))
+        self.play(mn.Write(subgroup_label))
+        self.play(mn.GrowFromCenter(brace_right), mn.GrowFromCenter(brace_down))
+        self.end_fragment()
+
+        first_circle_group = mn.VGroup()
+        first_circle_group.add(
+                circle_1,
+                circle_1_label,
+                rect_1,
+                brace_right,
+                brace_down,
+                subgroup_label
+                )
+
+        subgroup_group = mn.VGroup()
+        subgroup_group.add(
+                rect_1,
+                brace_right,
+                brace_down,
+                subgroup_label
+                )
+        
+        self.play(first_circle_group.animate.shift(4*mn.LEFT))
+
+        arrow = mn.Arrow(start=mn.LEFT*0.8, end=mn.RIGHT*0.9)
+        arrow_label = mn.Tex(r"$\alpha$")
+        arrow_label.move_to(mn.UP*0.5)
+        self.play(mn.LaggedStart(mn.GrowArrow(arrow), mn.Write(arrow_label), lag_ratio=0.5))
+
+        circle_2 = mn.Circle(radius=3, color=mn.PURE_GREEN, stroke_color=mn.WHITE, fill_opacity=0.6)
+        circle_2.move_to(4*mn.RIGHT)
+        circle_2_label = mn.Tex(r"$\alpha(G)$", font_size=36)
+        circle_2_label.move_to(circle_2.get_center() + (mn.UP + mn.LEFT)*3/np.sqrt(2)*1.2)
+        rect_2 = mn.Rectangle(height=2, width=2, fill_color=mn.GREY, fill_opacity=1)
+        rect_2.move_to(4*mn.RIGHT + mn.DOWN)
+        rect_2.align_to(rect_1, mn.DOWN)
+
+        brace_r_2 = mn.BraceLabel(rect_2, r"\alpha(U_+)", brace_direction=mn.RIGHT, font_size=36)
+        brace_d_2 = mn.BraceLabel(rect_2, r"\alpha(U_-)", brace_direction=mn.DOWN, font_size=36)
+        corner = rect_2.get_corner(mn.UP + mn.LEFT)
+        subgroup_label_2 = mn.Tex(r"$\alpha(U)$", font_size=36)
+        subgroup_label_2.move_to(corner + (mn.UP + mn.LEFT)*0.3)
+
+        group_2 = mn.VGroup()
+        group_2.add(
+                circle_2,
+                circle_2_label,
+                rect_2,
+                brace_r_2,
+                brace_d_2,
+                subgroup_label_2
+                )
+
+        self.play(mn.Create(circle_2), mn.Write(circle_2_label))
+        self.end_fragment()
+
+        rect_1_new = rect_1.copy()
+
+        self.play(mn.Transform(rect_1_new, rect_2))
+        self.play(mn.Write(subgroup_label_2))
+        self.play(mn.GrowFromCenter(brace_r_2), mn.GrowFromCenter(brace_d_2))
+        self.end_fragment()
+
+
+
+
+
+
+
+
