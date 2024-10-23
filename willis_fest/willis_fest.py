30117103da5b8bf7c1bea0f373dc220d88cc413b
Marcus <darthmonks@gmail.com>
2024-10-23 16:44:12 +1100

    Willis Fest Start

---
+++ willis_fest/willis_fest.py
@@ -0,0 +1,55 @@
+import manim as mn
+import numpy as np
+from manim_revealjs import PresentationScene, COMPLETE_LOOP
+
+
+mn.config.video_dir= "./videos"
+
+
+class IntroText(PresentationScene):
+    def construct(self):
+        title = mn.Tex("The ", "Scale Function ", "Values of ", "$(P)$-closed Groups ", r"\\Acting On Trees")
+        self.play(mn.Write(title))
+        self.end_fragment()
+
+        self.play(mn.Indicate(title[3]))
+        self.end_fragment()
+
+        animations = []
+        for i in range(0, 5):
+            if i not in {1}:
+                animations.append(mn.FadeOut(title[i], scale=0.2))
+
+        animations.append(title[1].animate.move_to(mn.ORIGIN))
+        self.end_fragment()
+
+        self.play(mn.AnimationGroup(*animations))
+        self.end_fragment()
+
+        self.play(mn.FadeOut(title[1], scale=0.5))
+        self.end_fragment()
+
+class ScaleSection(PresentationScene):
+    def construct(self):
+
+        text_1 = r"The \textit{scale function} is defined on any t.d.l.c.\ group."
+        text_2 = r"It is defined by \[ s : \operatorname{Aut}(G) \to \mathbb{N} : \alpha \mapsto \min_{U \in \mathcal{B}(G)} |\alpha(U) : \alpha(U) \cap U |. \]"
+        text_3 = r"Can be defined as a function from $G$ to $\mathbb{N}$ by using inner automorphisms."
+        text_4 = r"The \textit{tidying procedure} can be used to find a minimising subgroup for $\alpha \in \mathcal{B}(G)$."
+        bullet_list = mn.BulletedList(text_1, text_2, text_3, text_4, height=2, width=10)
+        for row in bullet_list:
+            self.play(mn.Write(row))
+            self.end_fragment()
+
+        text_ul = mn.Underline(bullet_list)
+        text_ul_rect = mn.Rectangle(width=bullet_list.width, height=bullet_list.height*1.1)\
+                .next_to(text_ul, mn.DOWN, buff=0)\
+                .set_style(fill_opacity=1, stroke_width=0, fill_color=mn.BLACK)
+        text_fade_gp = mn.VGroup(text_ul, text_ul_rect)
+        self.play(mn.GrowFromCenter(text_ul), mn.GrowFromCenter(text_ul_rect))
+        self.add(text_fade_gp)
+        self.play(text_fade_gp.animate.shift(mn.UP*text_ul_rect.height))
+        self.play(mn.ShrinkToCenter(text_ul))
+        self.end_fragment()
+
+
