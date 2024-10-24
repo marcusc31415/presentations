b78c7f6e5bd6280e179e18b7c7a73b09a344873f
Marcus <darthmonks@gmail.com>
2024-10-24 16:12:24 +1100

    Permutation Topology

--- willis_fest/willis_fest.py
+++ willis_fest/willis_fest.py
@@ -21,7 +21,6 @@ class IntroText(PresentationScene):
                 animations.append(mn.FadeOut(title[i], scale=0.2))
 
         animations.append(title[1].animate.move_to(mn.ORIGIN))
-        self.end_fragment()
 
         self.play(mn.AnimationGroup(*animations))
         self.end_fragment()
@@ -37,9 +36,11 @@ class ScaleSection(PresentationScene):
         text_3 = r"Can be defined as a function from $G$ to $\mathbb{N}$ by using inner automorphisms."
         text_4 = r"The \textit{tidying procedure} can be used to find a minimising subgroup for $\alpha \in \mathcal{B}(G)$."
         bullet_list = mn.BulletedList(text_1, text_2, text_3, text_4, height=2, width=10)
+        bullet_list.scale(1.2)
         for row in bullet_list:
             self.play(mn.Write(row))
             self.end_fragment()
+
 
         text_ul = mn.Underline(bullet_list)
         text_ul_rect = mn.Rectangle(width=bullet_list.width, height=bullet_list.height*1.1)\
@@ -52,4 +53,12 @@ class ScaleSection(PresentationScene):
         self.play(mn.ShrinkToCenter(text_ul))
         self.end_fragment()
 
+
+class PermutationTopology(PresentationScene):
+    def construct(self):
+        vertices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+        edges = [(1, 2), (1, 3), (1, 4), (4, 5), (4, 6), (2, 7), (2, 8), (3, 9), (3, 10), (3, 11), (10, 12), (12, 13), (12, 14), (14, 15), (2, 16)]
+        graph = mn.Graph(vertices, edges, layout_scale=3).scale(2.5)
+        self.play(mn.Create(graph))
+        self.end_fragment()
 
